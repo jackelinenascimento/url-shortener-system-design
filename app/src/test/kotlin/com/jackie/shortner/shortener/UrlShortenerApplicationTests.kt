73c3b019e317406855e01dd6d1ce5f6aa386b1f8
Jackie Nascimento <jackeline_nascimento@hotmail.com>
2025-09-13 15:05:54 -0300

    initial commit

---
+++ app/src/test/kotlin/com/jackie/shortner/shortener/UrlShortenerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jackie.shortner.shortener
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UrlShortenerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
